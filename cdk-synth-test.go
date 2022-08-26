1574e37aaa9930b194a5eced9eb9c170d66bd678
whs <hswongac@gmail.com>
2022-08-26 06:40:58 -0400

    Test

--- cdk-synth-test.go
+++ cdk-synth-test.go
@@ -3,8 +3,9 @@ package main
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsimagebuilder"
 	"github.com/aws/constructs-go/constructs/v10"
-	// "github.com/aws/jsii-runtime-go"
+	"github.com/aws/jsii-runtime-go"
 )
 
 type CdkSynthTestStackProps struct {
@@ -24,6 +25,58 @@ func NewCdkSynthTestStack(scope constructs.Construct, id string, props *CdkSynth
 	// queue := awssqs.NewQueue(stack, jsii.String("CdkSynthTestQueue"), &awssqs.QueueProps{
 	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
 	// })
+	component := awsimagebuilder.NewCfnComponent(stack, jsii.String("Component"), &awsimagebuilder.CfnComponentProps{
+		Name: jsii.String("install-cardano-node"),
+		Platform: jsii.String("Linux"),
+		Version: jsii.String("1.0.0"),
+		Data: jsii.String(`schemaVersion: 1.0
+phases:
+  - name: build
+    steps:
+      - name: InstallAnsible
+        action: ExecuteBash
+        inputs:
+          commands:
+          - sudo amazon-linux-extras install -y ansible2
+      - name: InstallGit
+        action: ExecuteBash
+        inputs:
+          commands:
+          - sudo yum install -y git
+      - name: InstallCardanoNode
+        action: ExecuteBash
+        inputs:
+          commands:
+          - |
+            set -x
+            sudo su -l ec2-user -c '(
+            t=$(mktemp -d)
+            git clone --recurse-submodules https://github.com/whslabs/packer-cardano-node.git $t
+            cd $t/ansible/
+            ansible-playbook -idefault, -clocal playbook.yaml
+            )'
+`),
+	})
+
+	awsimagebuilder.NewCfnImageRecipe(stack, jsii.String("ImageRecipe"), &awsimagebuilder.CfnImageRecipeProps{
+		Name: jsii.String("cardano-nodes"),
+		ParentImage: jsii.String("arn:aws:imagebuilder:us-east-1:aws:image/amazon-linux-2-x86/x.x.x"),
+		Version: jsii.String("1.0.0"),
+		BlockDeviceMappings: []interface{}{
+			&awsimagebuilder.CfnImageRecipe_InstanceBlockDeviceMappingProperty{
+				DeviceName: jsii.String("/dev/xvda"),
+				Ebs: &awsimagebuilder.CfnImageRecipe_EbsInstanceBlockDeviceSpecificationProperty{
+					DeleteOnTermination: jsii.Bool(false),
+					VolumeSize: jsii.Number(20),
+				},
+			},
+		},
+		Components: []interface{}{
+			&awsimagebuilder.CfnImageRecipe_ComponentConfigurationProperty{
+				ComponentArn: component.AttrArn(),
+			},
+		},
+	})
 
 	return stack
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module cdk-synth-test
 go 1.18
 
 require (
-  github.com/aws/aws-cdk-go/awscdk/v2 v2.39.0
-  github.com/aws/constructs-go/constructs/v10 v10.0.5
-  github.com/aws/jsii-runtime-go v1.29.0
+	github.com/aws/aws-cdk-go/awscdk/v2 v2.39.0
+	github.com/aws/constructs-go/constructs/v10 v10.1.85
+)
+
+require (
+	github.com/Masterminds/semver/v3 v3.1.1 // indirect
+	github.com/aws/jsii-runtime-go v1.65.0 // indirect
 )
